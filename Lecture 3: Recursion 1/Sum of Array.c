b571ac5cd3d120ab98c008969c29d36e3e5ec8f0
kanishk gaur <71584512+kanishk1511@users.noreply.github.com>
2022-03-11 13:15:54 +0530

    Create Sum of Array.c

---
+++ Lecture 3: Recursion 1/Sum of Array.c
@@ -0,0 +1,14 @@
+int sum(int input[], int n) {
+   if(n <= 0){
+       return 0;
+       
+   }
+    
+    return (sum(input , n - 1) + input[n - 1]);
+  /* Don't write main().
+     Don't read input, it is passed as function argument.
+     Return output and don't print it.
+     Taking input and printing output is handled automatically.
+  */
+
+}
